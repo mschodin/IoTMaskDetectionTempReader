ad5abb64a03401ee8097d132bf81d91e42167028
rdfarwell <rdeanfarwell3@gmail.com>
2021-04-28 00:29:57 -0500

    integrated temp with main
    integrated temp with main, added thermal camera imports (top 4), updated LED pins, changed GPIO.setmode, changed scan_person to check against global fever variable

--- mdtr.py
+++ mdtr.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
 
 # Imports
+import time
+import busio
+import board
+import adafruit_amg88xx
 from time import sleep
 from datetime import datetime
 import RPi.GPIO as GPIO
@@ -9,15 +13,19 @@ import RPi.GPIO as GPIO
 
 
 # Initialize outputs for green and red leds
-# TODO: Change pin below to correct pin
-green_LED = 18
-red_LED = 19
-GPIO.setmode(GPIO.BOARD)
+i2c = busio.I2C(board.SCL, board.SDA)
+amg = adafruit_amg88xx.AMG88XX(i2c)
+threshold = 32 #celcius, F = ~90
+fever = 38 # F = ~100
+green_LED = 27
+red_LED = 17
+GPIO.setmode(GPIO.BCM)
 GPIO.setup(green_LED, GPIO.OUT)
 GPIO.setup(red_LED, GPIO.OUT)
 
 # Main loop that runs in a while loop until scan is ready
 def main():
+    print("test")
     temp = 0
     while temp < 95:
         # TODO: scan for high temp
@@ -29,9 +37,10 @@ def main():
 # Collects mask data, temp data, time of day, and determines if entry is allowed. Pushes info to db
 def scan_person():
     average_temp = read_average_temp()
+    print(average_temp)
     has_mask = check_for_mask()
     allow_entry = True
-    if average_temp >= 100 or has_mask == False:
+    if average_temp >= fever or has_mask == False:
         allow_entry = False
     current_time = datetime.now()
     # TODO: Upload current_time, average_temp, has_mask, and allow_entry to database
@@ -47,11 +56,20 @@ def scan_person():
     
 # TODO: Reads average temperature of hottest point in scan
 def read_average_temp():
-    return 101
+    average = 0.0
+    counter = 0
+    for row in amg.pixels:
+        for temp in row:
+            if temp > threshold:
+                average += temp
+                counter += 1
+    if counter != 0:
+        average = average/counter
+    return average
 
 # TODO: Reads from camera and determines if scanned person is wearing a mask, returns true if mask is worn, false if not
 def check_for_mask():
-    return False
+    return True
 
 if __name__ == "__main__":
     main()
