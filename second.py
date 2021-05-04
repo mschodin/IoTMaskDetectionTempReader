9a6fd6a3413d852002deb9df5cd1f4d2fd99e6d2
awjung <73133056+awjung@users.noreply.github.com>
2021-05-04 16:11:47 -0500

    Update to second scripte

--- second.py
+++ second.py
@@ -12,9 +12,9 @@ args = parser.parse_args()
 
 
 # InfluxDB Init
-token = "P7SENJDBCC_VSxiaHTP6phlnljd4ubocxnN4KR7s2uVjmjW_vJVLNXWlQFQpCk9onbFCYEW8dG7-MWFYhTUg3Q=="
+token = "6ZIma7oJs7RJRs2yX_iKrSLG-fbeXprCVYA1Lxw-nAf4YhN6TF1ldvCCNUz5cMl67fdfkObltP1tg6LYoGYxyQ=="
 org = "awjung2000@comcast.net"
-bucket = "IoTProject"
+bucket = "IoT Presentation"
 
 client = InfluxDBClient(url="https://us-central1-1.gcp.cloud2.influxdata.com/", token=token)
 
@@ -25,5 +25,7 @@ average_temp = args.temp
 has_mask = args.mask
 print(average_temp)
 print(has_mask)
-point = Point("mem").tag("host", "host1").field("temperature",average_temp,"mask",has_mask).time(datetime.utcnow(), WritePrecision.NS)
-write_api.write(bucket, org, point)
+
+mask = 1 if has_mask else 0
+data = "mem,host=host1 hasmask={0},temperature={1}".format(mask, average_temp)
+write_api.write(bucket, org, data)
